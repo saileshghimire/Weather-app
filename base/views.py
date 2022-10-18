a134697a8fee0aa591d72fd1e657e9872fa4f57e
saileshghimire <saileshghimire1@gmail.com>
2022-10-18 15:21:31 +0545

    first commit

---
+++ base/views.py
@@ -0,0 +1,23 @@
+import urllib.request
+from django.shortcuts import render
+import json
+
+# Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+        source = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=14467c57e1a9e1a2a56503b88cff3b0b').read()
+        list_of_data = json.loads(source)
+        data = {
+            "country_code": str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon']) + ' '
+                        + str(list_of_data['coord']['lat']),
+            "temp": str(list_of_data['main']['temp']) + 'k',
+            "pressure": str(list_of_data['main']['pressure']),
+            "humidity": str(list_of_data['main']['humidity']),
+        }
+        print(data)
+    else:
+        data ={}
+    return render(request, "index.html", data)
